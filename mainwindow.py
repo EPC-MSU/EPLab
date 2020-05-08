d14318779f14f2e6346a973de580ef60309bc979
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-09 00:40:59 +0300

    Add IV window (empty)

--- mainwindow.py
+++ mainwindow.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QWidget, QFil
 from PyQt5.QtGui import QIcon
 import epcore.filemanager as epfilemanager
 from boardwindow import BoardWidget
+from ivviewer import Viewer as IVViewer
 
 
 class EPLabWindow(QMainWindow):
@@ -25,10 +26,19 @@ class EPLabWindow(QMainWindow):
         layout.addWidget(self._board_view_btn)
         self._board_view_btn.clicked.connect(self._on_view_board)
 
+        self._iv_view_btn = QPushButton("View IVC")
+        layout.addWidget(self._iv_view_btn)
+        self._iv_view_btn.clicked.connect(self._on_view_iv)
+
         self._board_window = BoardWidget()
         self._board_window.resize(600, 600)
         self._board_window.setWindowIcon(QIcon("media/ico.png"))
         self._board_window.setWindowTitle("EPLab - Board")
+
+        self._iv_window = IVViewer()
+        self._iv_window.resize(200, 200)
+        self._iv_window.setWindowIcon((QIcon("media/ico.png")))
+        self._iv_window.setWindowTitle("EPLab - IVC")
 
     def _on_load_board(self):
         dialog = QFileDialog()
@@ -38,3 +48,6 @@ class EPLabWindow(QMainWindow):
 
     def _on_view_board(self):
         self._board_window.show()
+
+    def _on_view_iv(self):
+        self._iv_window.show()
