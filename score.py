699989b14ae66698ad1fde9c3fcf0ac1d5fc1b6f
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-13 20:59:33 +0300

    Fix score

--- score.py
+++ score.py
@@ -11,7 +11,7 @@ class ScoreWrapper:
 
     def set_score(self, score: float):
 
-        color = self._color_good if score > self._treshold else self._color_bad
+        color = self._color_good if score < self._treshold else self._color_bad
         friendly_score = round(score * 100.0)
         text = f'<html><head/><body><p><span style="font-size:48pt;color:{color};">' \
                f'{friendly_score}%</span></p></body></html>'
