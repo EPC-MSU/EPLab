3913069bc76525c3c956118eb37b492cae00c294
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-13 19:12:42 +0300

    Virtual IVC works

--- mainwindow.py
+++ mainwindow.py
@@ -1,18 +1,28 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QWidget, QFileDialog, QLabel
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QTimer
 from PyQt5 import uic
+
 import epcore.filemanager as epfilemanager
+from epcore.measurementmanager import MeasurementSystem
+from epcore.elements import IVCurve
 from boardwindow import BoardWidget
 from boardwindow import GraphicsManualPinItem
-from ivviewer import Viewer as IVViewer
+from ivviewer import Viewer as IVViewer, Curve as ViewerCurve
+
+
+def _to_viewer_curve(curve: IVCurve) -> ViewerCurve:
+    return ViewerCurve(x=curve.voltages, y=curve.currents)
 
 
 class EPLabWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, msystem: MeasurementSystem):
         super(EPLabWindow, self).__init__()
 
-        uic.loadUi("gui/mainwindow.ui", self)  # Load the .ui file
+        uic.loadUi("gui/mainwindow.ui", self)
+
+        self._msystem = msystem
+        self._msystem.trigger_measurements()
 
         self._board_window = BoardWidget()
         self._board_window.resize(600, 600)
@@ -25,6 +35,20 @@ class EPLabWindow(QMainWindow):
         self._iv_window.setWindowTitle("EPLab - IVC")             # TODO: don't duplicate base configurations
 
         self.setCentralWidget(self._iv_window)
+
+        QTimer.singleShot(0, self._update_ivc)
+
+    @pyqtSlot()
+    def _update_ivc(self):
+        if self._msystem.measurements_are_ready():
+            self._msystem.trigger_measurements()
+            # TODO: why [0] measurer is 'ref' and [1] measurer is 'test'? Should be smth like measurers.test
+            ref = _to_viewer_curve(self._msystem.measurers[0].get_last_iv_curve())
+            test = _to_viewer_curve(self._msystem.measurers[1].get_last_iv_curve())
+            self._iv_window.plot.set_reference_curve(ref)
+            self._iv_window.plot.set_test_curve(test)
+
+            QTimer.singleShot(0, self._update_ivc)
 
     @pyqtSlot()
     def _on_load_board(self):
