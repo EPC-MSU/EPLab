5b4d1deb46f0fc63ee6727e75a479f76452fe8ed
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-20 18:40:43 +0300

    Add translator

---
+++ gui/super_translate_en.ts
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="en_US" sourcelanguage="">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="pcmainwindow.ui" line="14"/>
+        <source>Eyepoint S1</source>
+        <translation>Eyepoint S1</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="152"/>
+        <source>Устройства не подключены</source>
+        <translation>Devices not connected</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="260"/>
+        <source>Файл не найден</source>
+        <translation>File not found</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="434"/>
+        <source>Частота пробного сигнала</source>
+        <translation>Probe signal frequency</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="477"/>
+        <source>10 кГц</source>
+        <translation>10 kHz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="453"/>
+        <source>10 Гц</source>
+        <translation>10 Hz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="463"/>
+        <source>100 Гц</source>
+        <translation>100 Hz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="470"/>
+        <source>1 кГц</source>
+        <translation>1 kHz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="539"/>
+        <source>Амплитуда пробного сигнала</source>
+        <translation>Probe signal amplitude</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="552"/>
+        <source>3.3 В</source>
+        <translation>3.3 V</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="562"/>
+        <source>5 В</source>
+        <translation>5 V</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="569"/>
+        <source>12 В</source>
+        <translation>12 V</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="500"/>
+        <source>Чувствительность по току</source>
+        <translation>Current sensitivity</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="embeddedmainwindow.ui" line="414"/>
+        <source>Низкая</source>
+        <translation>Low</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="embeddedmainwindow.ui" line="451"/>
+        <source>Высокая</source>
+        <translation>High</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="embeddedmainwindow.ui" line="488"/>
+        <source>Средняя</source>
+        <translation>Normal</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="354"/>
+        <source>Степень различия</source>
+        <translation>Difference</translation>
+    </message>
+    <message>
+        <location filename="embeddedmainwindow.ui" line="312"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:48pt; color:#73d216;&quot;&gt;0.00&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:48pt; color:#73d216;&quot;&gt;0.00&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="615"/>
+        <source>Фиксировать A</source>
+        <translation>Freeze A</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="625"/>
+        <source>Фиксировать B</source>
+        <translation>Freeze B</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="833"/>
+        <source>Сохранить изображение</source>
+        <translation>Save image</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="795"/>
+        <source>&lt;</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="824"/>
+        <source>&gt;</source>
+        <translation></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="733"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="740"/>
+        <source>Новая точка</source>
+        <translation>New point</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="865"/>
+        <source>Автокалибровка</source>
+        <translation>Autocalibration</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="embeddedmainwindow.ui" line="994"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Порог&lt;br/&gt;ошибки&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Diff.&lt;br/&gt;threshold&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="893"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="928"/>
+        <source>+</source>
+        <translation>+</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="embeddedmainwindow.ui" line="198"/>
+        <source>Версии не совпадают</source>
+        <translation>Versions not compatible</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="484"/>
+        <source>100 кГц</source>
+        <translation>100 kHz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="270"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600; color:#ff0000;&quot;&gt;Версии не совпадают&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600; color:#ff0000;&quot;&gt;Versions do not match &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="381"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:48pt; color:#73d216;&quot;&gt;0%&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:48pt; color:#73d216;&quot;&gt;0%&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="966"/>
+        <source>FPS</source>
+        <translation>FPS</translation>
+    </message>
+    <message>
+        <location filename="pcmainwindow.ui" line="973"/>
+        <source>0.0</source>
+        <translation>0.0</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="403"/>
+        <source>Звук</source>
+        <translation>Sound</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="412"/>
+        <source>Включен</source>
+        <translation>Enabled</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="506"/>
+        <source>250 мкА</source>
+        <translation>250 uA</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="516"/>
+        <source>2.5 мА</source>
+        <translation>2.5 mA</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="523"/>
+        <source>25 мА</source>
+        <translation>25 mA</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="595"/>
+        <source>Сравнение</source>
+        <translation>Save</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="606"/>
+        <source>ВАХ</source>
+        <translation>IVC</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="547"/>
+        <source>Фиксировать А</source>
+        <translation type="obsolete">Freeze A</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="557"/>
+        <source>Фиксировать В</source>
+        <translation type="obsolete">Freeze B</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="635"/>
+        <source>Изображение</source>
+        <translation>Image</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="659"/>
+        <source>Запись</source>
+        <translation>Rec</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="779"/>
+        <source>Тестирование</source>
+        <translation>Test</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="859"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="874"/>
+        <source>Порог ошибки:</source>
+        <translation>Score threshold:</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="954"/>
+        <source>Отладка</source>
+        <translation>Debug</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="764"/>
+        <source>Сохранить в новый файл</source>
+        <translation>Save to new file</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="771"/>
+        <source>Открыть файл</source>
+        <translation>Open file</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="443"/>
+        <source>1 Гц</source>
+        <translation>1 Hz</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="545"/>
+        <source>1.2 В</source>
+        <translation>1.2 V</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="285"/>
+        <source>Все равно продолжить</source>
+        <translation>Continue anyway</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="pcmainwindow.ui" line="307"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600; color:#ff0000;&quot;&gt;Версии не совпадают&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600; color:#ff0000;&quot;&gt;Versions do not match&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context encoding="UTF-8">
+    <name>t</name>
+    <message encoding="UTF-8">
+        <location filename="ivcviewer.py" line="66"/>
+        <source>
+Напряжение (В)</source>
+        <translation>
+Voltage (V)</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="ivcviewer.py" line="78"/>
+        <source>Ток (мА)
+</source>
+        <translation>Current (mA)
+</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="322"/>
+        <source>Версия библиотеки </source>
+        <translation>Library version </translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="325"/>
+        <source>
+Версия прошивки: </source>
+        <translation>Firmware version: </translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="328"/>
+        <source>
+Эта версия библиотеки не подходит для данной прошивки.
+ Версии прошивок, которые совместимы с данной библиотекой:
+</source>
+        <translation>
+This library version not correspond for this firmware. 
+The firmware versions, which compatible with this library: </translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="335"/>
+        <source>За помощью обращайтесь в техническую поддержку
+ ЦИФ МГУ по email адресу:  info@ximc.ru</source>
+        <translation>For help, you need contact to technical support 
+EPC MSU e-mail:  info@ximc.ru</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="726"/>
+        <source>Сохранение изображения</source>
+        <translation>Save screen shot</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="727"/>
+        <source>Сохранение ВАХ</source>
+        <translation>Save IVC</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="728"/>
+        <source>Выбор файла с ВАХ</source>
+        <translation>Open file with IVC</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="mainwindow.py" line="343"/>
+        <source>Все равно продолжить</source>
+        <translation type="obsolete">Continue anyway</translation>
+    </message>
+</context>
+</TS>
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTranslator
 import sys
 import logging
+from argparse import ArgumentParser
 from epcore.ivmeasurer import IVMeasurerVirtual
 from epcore.measurementmanager import MeasurementSystem
 
@@ -8,9 +10,19 @@ from mainwindow import EPLabWindow
 
 
 if __name__ == "__main__":
+
+    parser = ArgumentParser(description="EyePoint Lab")
+    parser.add_argument("--en", help="Use English version", action="store_true")
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.ERROR)
 
     app = QApplication(sys.argv)
+
+    if args.en:
+        translator = QTranslator()
+        translator.load("gui/super_translate_en.qm")
+        app.installTranslator(translator)
 
     ivm1 = IVMeasurerVirtual()
     ivm2 = IVMeasurerVirtual()
