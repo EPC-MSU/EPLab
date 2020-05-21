9170ce28bec3e0b4e0ef5991dd8496053eda5ee5
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-21 19:12:43 +0300

    Add real measurers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import QTranslator
 import sys
 import logging
 from argparse import ArgumentParser
-from epcore.ivmeasurer import IVMeasurerVirtual
+from epcore.ivmeasurer import IVMeasurerVirtual, IVMeasurerIVM10
 from epcore.measurementmanager import MeasurementSystem
 
 from mainwindow import EPLabWindow
@@ -13,6 +13,8 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(description="EyePoint Lab")
     parser.add_argument("--en", help="Use English version", action="store_true")
+    parser.add_argument("-a", help="Path to measurer A", default="virtual")
+    parser.add_argument("-b", help="Path to measurer B", default="virtual")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.ERROR)
@@ -24,8 +26,15 @@ if __name__ == "__main__":
         translator.load("gui/super_translate_en.qm")
         app.installTranslator(translator)
 
-    ivm1 = IVMeasurerVirtual()
-    ivm2 = IVMeasurerVirtual()
+    if args.a == "virtual":
+        ivm1 = IVMeasurerVirtual()
+    else:
+        ivm1 = IVMeasurerIVM10(args.a)
+    if args.b == "virtual":
+        ivm2 = IVMeasurerVirtual()
+    else:
+        ivm2 = IVMeasurerIVM10(args.b)
+
     measurement_system = MeasurementSystem([ivm1, ivm2])
 
     window = EPLabWindow(measurement_system)
---
+++ run_real.bat
@@ -0,0 +1,5 @@
+set IVM_A=com:\\.\COM42
+set IVM_B=com:\\.\COM43
+
+
+python main.py -a %IVM_A% -b %IVM_B%
