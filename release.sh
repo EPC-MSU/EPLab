f609230cfffedc7cd9fc3451580adde7f1945ab3
mikheev <mikheev@physlab.ru>
2020-07-08 14:47:17 +0300

    flake8 config fixed.

---
+++ release.sh
@@ -0,0 +1,18 @@
+rm -rf venv
+rm -rf dist
+rm -rf build
+rm -rf release
+python -m venv venv
+./venv/bin/python -m pip install --upgrade pip
+./venv/bin/python -m pip install -r requirements.txt
+./venv/bin/python -m pip install pyinstaller
+./venv/bin/pyinstaller main.py \
+--add-data "./venv/lib/python3.6/site-packages/epcore/ivmeasurer/ivm-debian/libivm.so:." \
+--add-data "./venv/lib/python3.6/site-packages/epcore/doc/p10_elements.schema.json;epcore/doc" \
+--add-data "./venv/lib/python3.6/site-packages/epcore/doc/p10_elements_2.schema.json;epcore/doc" \
+--add-data "./venv/lib/python3.6/site-packages/epcore/doc/ufiv.schema.json;epcore/doc" \
+--add-data "./venv/lib/python3.6/site-packages/epcore/measurementmanager/ivcmp-debian/libivcmp.so:." \
+--add-data "./venv/lib/python3.6/site-packages/epsound/void.wav;epsound" \
+--add-data "./media/*:media" \
+--add-data "./gui/*:gui" \
+--icon=media/ico.ico
