e74329c15b313a2b0a6e681857464c193f385908
mikheev <mikheev@physlab.ru>
2020-12-11 09:55:21 +0300

    rebuild venv fixed.

--- rebuild_venv.bat
+++ rebuild_venv.bat
@@ -1,4 +1,4 @@
-rd /S /Q venv
-C:\Python36\python -m venv venv
+if exist venv rd /S /Q venv
+python -m venv venv
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
