d01faae974a0b3f1aeac3b136e6a9367c1d5759b
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-05-19 09:19:07 +0300

    Changed running tests on Linux

--- testall.sh
+++ testall.sh
@@ -3,6 +3,7 @@ rm -rf venv
 set -e
 
 python3.6 -m venv venv
+./venv/bin/python -m pip install --upgrade pip
 ./venv/bin/python -m pip install -r requirements.txt
 
 export PYTHONPATH=$(dirname "$0")
