f8d5271edccf1b05775007ae36801a5ba4e21a34
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-06-11 10:45:16 +0300

    New commit in not-dev branch to cheat flake8-checker

--- check_flake8.py
+++ check_flake8.py
@@ -4,4 +4,4 @@ if a is True: b = 8
 else: b = 9
 my_string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
-my_string = "sdsdssd"
+my_string = "sdsdss"
