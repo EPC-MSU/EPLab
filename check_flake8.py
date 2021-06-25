b7d4306c544e9119cdb01c0231212c10d8bf72be
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-06-25 14:26:45 +0300

    Deleted uncorrect flake8 file

--- check_flake8.py
+++
@@ -1,8 +0,0 @@
-a = True
-b = 8
-if a is True: b = 8
-else: b = 9
-my_string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-my_string = "b"
-
-my_string = "sdsdssd"
