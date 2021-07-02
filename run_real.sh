28b801bf34bb7ea29c2587357f35a1f57eba618f
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-07-02 10:42:01 +0300

    Added scripts to install and run application

---
+++ run_real.sh
@@ -0,0 +1,3 @@
+IVM_TEST="com:///dev/ttyACMx"
+IVM_REF="com:///dev/ttyACMx"
+./venv/bin/python3 main.py $IVM_TEST --ref $IVM_REF
--- run_virtual.bat
+++ run_virtual.bat
@@ -1,2 +1,2 @@
 set QT_QPA_PLATFORM_PLUGIN_PATH=venv\Lib\site-packages\PyQt5\Qt\plugins\platforms
-venv\scripts\python main.py virtual --ref virtual --en
+venv\Scripts\python main.py virtual --ref virtual --en
