f05537bc13c114a69b0b1680e2979992b949e20d
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-08-03 17:57:11 +0300

    Version decrement and changed release script

--- release.bat
+++ release.bat
@@ -13,14 +13,37 @@ if exist release rd /S /Q release
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 venv\Scripts\python -m pip install pyinstaller
-venv\Scripts\pyinstaller main.py ^
---add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm-win%target_platform%\ivm.dll;." ^
+if %target_platform%==32 venv\Scripts\pyinstaller main.py ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\asa.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_client.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_util.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_xmlparse.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_xmltok.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm02\ivm-win32\ivm.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm10\ivm-win32\ivm.dll;." ^
 --add-data "venv\Lib\site-packages\epcore\doc\p10_elements.schema.json;epcore\doc" ^
 --add-data "venv\Lib\site-packages\epcore\doc\p10_elements_2.schema.json;epcore\doc" ^
 --add-data "venv\Lib\site-packages\epcore\doc\ufiv.schema.json;epcore\doc" ^
 --add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
 --add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
---add-data "venv\Lib\site-packages\epcore\measurementmanager\ivcmp-win%target_platform%\ivcmp.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\measurementmanager\ivcmp-win32\ivcmp.dll;." ^
+--add-data "venv\Lib\site-packages\epsound\void.wav;epsound" ^
+--add-data "media\*;media" ^
+--add-data "gui\*;gui" ^
+--add-data "cur.ini;." ^
+--hidden-import=PyQt5.sip ^
+--icon=media\ico.ico
+
+if %target_platform%==64 venv\Scripts\pyinstaller main.py ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm02\ivm-win64\ivm.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm10\ivm-win64\ivm.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\doc\p10_elements.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\doc\p10_elements_2.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\doc\ufiv.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
+--add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
+--add-data "venv\Lib\site-packages\epcore\measurementmanager\ivcmp-win64\ivcmp.dll;." ^
 --add-data "venv\Lib\site-packages\epsound\void.wav;epsound" ^
 --add-data "media\*;media" ^
 --add-data "gui\*;gui" ^
@@ -29,6 +52,7 @@ venv\Scripts\pyinstaller main.py ^
 --icon=media\ico.ico
 
 xcopy release_templates\win%target_platform%\* dist\* /S /E
+copy eplab_asa_oprtions.json release\eplab
 rename dist release
 cd release
 rename main eplab
