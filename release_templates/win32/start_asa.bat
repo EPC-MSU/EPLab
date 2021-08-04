8e9db5346a912e18a60bb4fe105f08002e7d4fe4
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-08-04 08:03:47 +0300

    Changed script to run app with asa device

--- release_templates/win32/start_asa.bat
+++ release_templates/win32/start_asa.bat
@@ -1,3 +1,3 @@
 cd eplab
-main.exe xmlrpc:172.16.128.137 --config eplab_asa_options.json
+main.exe xmlrpc:172.16.128.137 --ref virtualasa --config eplab_asa_options.json
 pause
--- release_templates/win64/start_asa.bat
+++
@@ -1,3 +0,0 @@
-cd eplab
-main.exe xmlrpc:172.16.128.137 --config eplab_asa_options.json
-pause
