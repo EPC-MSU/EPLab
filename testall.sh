cc017b89a4ccf4f6ef4b5905036cebcd7892761c
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-11-09 09:02:03 +0300

    Fixed script to run tests in Linux

--- testall.sh
+++ testall.sh
@@ -1,10 +1,6 @@
 rm -rf venv
-
-set -e
-
-python3.6 -m venv venv
-./venv/bin/python -m pip install -r requirements.txt
-
+python3 -m venv venv
+./venv/bin/python3 -m pip install --upgrade pip
+./venv/bin/python3 -m pip install -r requirements.txt
 export PYTHONPATH=$(dirname "$0")
-
-./venv/bin/python test.py tests
+./venv/bin/python3 test.py tests
