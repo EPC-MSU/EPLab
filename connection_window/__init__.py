6cd595a0a977f6984c93ada39b619422294cf6e8
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2022-05-20 15:31:37 +0300

    Fixed flake8 error

--- connection_window/__init__.py
+++ connection_window/__init__.py
@@ -2,7 +2,7 @@
 Module for dialog window to select available measurers and multiplexer devices.
 """
 
-import connection_window.utils
+import connection_window.utils as utils
 from connection_window.connection_window import ConnectionWindow
 from connection_window.utils import ProductNames
 
