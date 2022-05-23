a3d7a0fab6b1aa0e9940103ed312dcb6f4fd1156
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2022-05-23 18:03:58 +0300

    Changed scripts to release

--- release.bat
+++ release.bat
@@ -14,6 +14,11 @@ venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 venv\Scripts\python -m pip install pyinstaller
 if %target_platform%==32 venv\Scripts\pyinstaller main.py --clean --onefile --noconsole ^
+--add-data "venv\Lib\site-packages\epcore\analogmultiplexer\epmux\epmux-win32\epmux.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\doc\p10_elements.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\doc\p10_elements_2.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\doc\ufiv.schema.json;epcore\doc" ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ASA_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\asa.dll;." ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc.dll;." ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_client.dll;." ^
@@ -22,46 +27,43 @@ if %target_platform%==32 venv\Scripts\pyinstaller main.py --clean --onefile --no
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\asa10\libasa-win32\libxmlrpc_xmltok.dll;." ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm02\ivm-win32\ivm.dll;epcore\ivmeasurer\ivm02" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm10\ivm-win32\ivm.dll;epcore\ivmeasurer\ivm10" ^
---add-data "venv\Lib\site-packages\epcore\ivmeasurer\ASA_device_settings.json;epcore\ivmeasurer" ^
---add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_virtual_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_settings.json;epcore\ivmeasurer" ^
---add-data "venv\Lib\site-packages\epcore\doc\p10_elements.schema.json;epcore\doc" ^
---add-data "venv\Lib\site-packages\epcore\doc\p10_elements_2.schema.json;epcore\doc" ^
---add-data "venv\Lib\site-packages\epcore\doc\ufiv.schema.json;epcore\doc" ^
---add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
---add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_virtual_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\measurementmanager\ivcmp-win32\ivcmp.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
+--add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
 --add-data "venv\Lib\site-packages\epsound\void.wav;epsound" ^
 --add-data "venv\Lib\site-packages\ivviewer\media\*;ivviewer\media" ^
 --add-data "venv\Lib\site-packages\report_templates\*;report_templates" ^
+--add-data "cur.ini;." ^
+--add-data "gui\*;gui" ^
+--add-data "media\*;media" ^
 --add-data "resources\eplab_asa_options.json;resources" ^
 --add-data "resources\win32\*;resources\win32." ^
---add-data "media\*;media" ^
---add-data "gui\*;gui" ^
---add-data "cur.ini;." ^
 --hidden-import=PyQt5.sip ^
 --icon media\ico.ico
 
 if %target_platform%==64 venv\Scripts\pyinstaller main.py --clean --onefile --noconsole ^
+--add-data "venv\Lib\site-packages\epcore\analogmultiplexer\epmux\epmux-win64\epmux.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\ASA_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm02\ivm-win64\ivm.dll;epcore\ivmeasurer\ivm02" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\ivm10\ivm-win64\ivm.dll;epcore\ivmeasurer\ivm10" ^
---add-data "venv\Lib\site-packages\epcore\ivmeasurer\ASA_device_settings.json;epcore\ivmeasurer" ^
---add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_virtual_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_settings.json;epcore\ivmeasurer" ^
+--add-data "venv\Lib\site-packages\epcore\ivmeasurer\EyePoint_virtual_device_settings.json;epcore\ivmeasurer" ^
 --add-data "venv\Lib\site-packages\epcore\doc\p10_elements.schema.json;epcore\doc" ^
 --add-data "venv\Lib\site-packages\epcore\doc\p10_elements_2.schema.json;epcore\doc" ^
 --add-data "venv\Lib\site-packages\epcore\doc\ufiv.schema.json;epcore\doc" ^
---add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
---add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
 --add-data "venv\Lib\site-packages\epcore\measurementmanager\ivcmp-win64\ivcmp.dll;." ^
+--add-data "venv\Lib\site-packages\epcore\product\doc\eplab_schema.json;epcore\product\doc" ^
+--add-data "venv\Lib\site-packages\epcore\product\eplab_default_options.json;epcore\product" ^
 --add-data "venv\Lib\site-packages\epsound\void.wav;epsound" ^
 --add-data "venv\Lib\site-packages\ivviewer\media\*;ivviewer\media" ^
 --add-data "venv\Lib\site-packages\report_templates\*;report_templates" ^
+--add-data "cur.ini;." ^
+--add-data "gui\*;gui" ^
+--add-data "media\*;media" ^
 --add-data "resources\eplab_asa_options.json;resources" ^
 --add-data "resources\win64\*;resources\win64." ^
---add-data "media\*;media" ^
---add-data "gui\*;gui" ^
---add-data "cur.ini;." ^
 --hidden-import=PyQt5.sip ^
 --icon media\ico.ico
 
