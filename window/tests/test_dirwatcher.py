a2c5c8b8188dd7f0e423ad5912b5b28717b54b18
timerke <timerke@mail.ru>
2023-11-16 16:43:59 +0300

    Added test

--- window/tests/test_dirwatcher.py
+++ window/tests/test_dirwatcher.py
@@ -14,7 +14,12 @@ class TestDirWatcher(unittest.TestCase):
 
         self._dir_watcher = DirWatcher(self._dir_base)
 
-    def test_creation(self) -> None:
-        for dir_name in ("reference", "reports", "screenshot"):
+    def test_path_request(self) -> None:
+        for dir_name in ("Reference", "Reports", "Screenshot"):
+            dir_path = getattr(self._dir_watcher, dir_name.lower(), None)
+            self.assertEqual(os.path.join(self._dir_base, "EPLab-Files", dir_name), dir_path)
+
+    def test_tree_creation(self) -> None:
+        for dir_name in ("Reference", "Reports", "Screenshot"):
             dir_path = getattr(self._dir_watcher, dir_name, None)
             self.assertTrue(os.path.exists(dir_path))
