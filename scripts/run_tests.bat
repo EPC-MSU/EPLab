a637ab7e86ceac36bd38faa8629a20e32cb5b157
timerke <timerke@mail.ru>
2023-12-13 17:30:22 +0300

    Added check on flake8 at test script

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,4 +1,8 @@
 cd ..
 echo --- Run all tests ---
 venv\Scripts\python -m unittest discover -v
+echo --- Check flake8 ---
+venv\Scripts\python -m pip install flake8
+venv\Scripts\python -m flake8 .
+echo --- Done ---
 pause
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,7 @@
 cd ..
 echo "--- Run all tests ---"
 ./venv/bin/python3 -m unittest discover -v
+echo "--- Check flake8 ---"
+./venv/bin/python3 -m pip install flake8
+./venv/bin/python3 -m flake8 .
+echo "--- Done ---"
