20e8371ecf09dd7b714087e325751ed1dd7b6ff6
timerke <timerke@mail.ru>
2023-12-21 13:15:05 +0300

    Fixed test

--- dialogs/tests/test_keymapdialog.py
+++ dialogs/tests/test_keymapdialog.py
@@ -1,12 +1,20 @@
 import sys
 import unittest
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QLabel
 from dialogs.keymapdialog import KeymapDialog
 
 
 class TestKeymapDialog(unittest.TestCase):
 
+    class MainWindow:
+
+        def __init__(self) -> None:
+            self.save_point_action = QLabel("some text")
+
+    def setUp(self) -> None:
+        self.app: QApplication = QApplication(sys.argv[1:])
+        self.main_window = self.MainWindow()
+
     def test_keymap_dialog(self) -> None:
-        _ = QApplication(sys.argv[1:])
-        window = KeymapDialog()
+        window = KeymapDialog(self.main_window)
         self.assertIsNotNone(window)
