979cbee65f68116c63ea4ab2430e623df64b8b23
timerke <timerke@mail.ru>
2026-02-06 13:26:23 +0300

    Fixed import ivm from epcore #122577

--- window/common.py
+++ window/common.py
@@ -1,7 +1,7 @@
 from enum import auto, Enum
 from epcore.analogmultiplexer.epmux.epmux import UrpcDeviceUndefinedError as MuxUrpcDeviceUndefinedError
 from epcore.ivmeasurer.asa10.asa import AsaConnectionError, AsaServerResponseError
-from ivm import UrpcDeviceUndefinedError
+from epcore.ivmeasurer.ivm import UrpcDeviceUndefinedError
 
 
 class WorkMode(Enum):
